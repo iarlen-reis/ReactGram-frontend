75b86e6bb38a743025c1aa6526e8302aaddf3846
= <iarlensr@hotmail.com>
2023-02-18 16:33:03 -0300

    Criando service redux da autentificação de registro de usuário.

---
+++ src/services/authService.js
@@ -0,0 +1,27 @@
+import { api, requestConfig } from "../utils/config";
+
+// Register an user
+const register = async (data) => {
+  // Configurando a requisição http
+  const config = requestConfig("POST", data);
+
+  try {
+    // Fazendo a requisição e pegando o dado.
+    const response = await fetch(api + "/users/register", config).then(
+      (response) => response.json().catch((error) => error)
+    );
+
+    // Se o dado retornar (Usuário e token) salvo no localstorage.
+    if (response) {
+      localStorage.setItem("user", JSON.stringify(response));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const authService = {
+  register,
+};
+
+export default authService;
