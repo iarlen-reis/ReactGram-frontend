80f2b820c5d536b6be352f7fd197760fcd5ea1dc
= <iarlensr@hotmail.com>
2023-02-18 16:24:28 -0300

    Criando pasta utils e adicionando o config.js, que será responsavel por gerenciar as requisições HTTP na API.

---
+++ src/utils/config.js
@@ -0,0 +1,34 @@
+export const api = "https://www.reactgram-backend.iarlenreis.com.br/api";
+
+export const upload = "https://www.reactgram-backend.iarlenreis.com.br/uploads";
+
+export const requestConfig = (method, data, token = null, image = null) => {
+  let config;
+
+  if (image) {
+    config = {
+      method,
+      body: data,
+      headers: {},
+    };
+  } else if (method === "DELETE" || data === null) {
+    config = {
+      method,
+      headers: {},
+    };
+  } else {
+    config = {
+      method,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+};
